ba4a20cdb59ee949b016b2d65f7270d126c2fa46
Aditya Aman <adityaaman22948@gmail.com>
2025-06-28 15:31:16 +0530

    fronted

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_BASE || 'https://bookmanagementproject-backend.onrender.com',
 })
